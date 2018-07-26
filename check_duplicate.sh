12c5826a3d4ba895b85258732cfdf37147df5924
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2018-07-27 08:26:49 +0900

    Marge check_duplicate.sh and viewall.sh

--- check_duplicate.sh
+++ check_duplicate.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 # Check Duplicate Files by SHA1 hash
 
 # Check Args
-if [ $# -ne 1 ]; then
+if [ $# -ne 3 ] && [ $# -ne 2 ] && [ $# -ne 1 ]; then
+  echo "Check Duplicate Files by SHA1 hash\n" 1>&2
   echo "Usage:" 1>&2
-  echo "./check_duplicate.sh Directory" 1>&2
+  echo "./check_duplicate.sh Directory1 [Directory2] [Directory3]" 1>&2
   exit 1
 fi
 
-/usr/bin/find $1 -type f -name '*' -exec sha1sum {} \; | sort -k 1 | uniq -Dw 40
+/usr/bin/find $1 $2 $3 -type f | xargs -d '\n' sha1sum| sort -k 1 | uniq -Dw 40
