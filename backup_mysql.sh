78a890687964acbc32803af00a3ef7e07c42d1f1
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2019-02-14 23:45:27 +0900

    fix backup_mysql.sh. backup from docker based server. use password.conf file.

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+cd $(dirname $0)
+. password.conf
 
-/usr/bin/mysqldump -A -u root --single-transaction --opt > /mnt/backup/mysql_`date +%Y%m%d`.sql
+/usr/bin/docker exec -i mariadb /bin/sh -c "/usr/bin/mysqldump -A -u root --single-transaction --opt --password=$MYSQL_PASSWORD |bzip2 > /mariadb.sql.bz2"
+/usr/bin/docker cp mariadb:/mariadb.sql.bz2 /mnt/ftp-remote/temp/mariadb_`date +%Y%m%d`.sql.bz2
+
