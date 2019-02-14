e70e65a66d990757949ab7fde1d8e6d3112886ae
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2019-02-14 20:33:33 +0900

    add full backup handling to backup_hanoi_rotate.sh

--- backup_hanoi_rotate.sh
+++ backup_hanoi_rotate.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 DAYS=$(/root/centos-admin-script/HanoiBackup/hanoi)
+BASEDIR=/mnt/ftp-remote
+
 rm -rf /mnt/ftp-remote/keep${DAYS}day
 mv /mnt/ftp-remote/temp /mnt/ftp-remote/keep${DAYS}day
-mkdir /mnt/ftp-remote/temp
+mkdir $BASEDIR/temp
+
+# When 1st day of the month, Handle backup as full backup.
+TODAY=$(date +%d)
+if [ $TODAY = "1" ]; then
+  cp $BASEDIR/keep1day/* $BASEDIR/full-backup/
+fi
